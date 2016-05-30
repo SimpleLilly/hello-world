6166a342433ef3b2e2c7c0f290f5374e6c4685e3
Lily.Chen <lily.chen@safenet-inc.com>
2016-05-30 17:31:15 +0800

    buildno.py on orion-6

---
+++ buildno.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#encoding:gb2312
+
+import httplib, urllib, urllib2, string, ctypes
+
+httpClient = None
+try:
+  httpClient = httplib.HTTPConnection("orion-6", 80, timeout=30)
+  httpClient.request("POST", "/buildno.txt")
+  response = httpClient.getresponse()
+  print response.read()
+  BUILD_ID=urllib.urlopen("http://orion-6:80/buildno.txt").read()
+  NEXT_ID = int(BUILD_ID)+1
+#  print NEXT_ID
+  f=open('/data/www/htdocs/buildno.txt','w')
+  f.write(str(NEXT_ID))
+  f.close()
+
+except Exception, e:
+  print e
+finally:
+  if httpClient:
+    httpClient.close()
